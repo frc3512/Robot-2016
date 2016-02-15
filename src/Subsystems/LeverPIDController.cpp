b762015833efb94a19409f58cc8089a46013ad6c
KeepEnemiesCloser <codelyokoliker@gmail.com>
2016-02-15 14:00:29 -0800

    Added LeverPIDController for shooter height and updated CANTalon IDs
    Change-Id: Ic271e3674a934fdb40048df9639c71b3acb1ef41

---
+++ src/Subsystems/LeverPIDController.cpp
@@ -0,0 +1,31 @@
+// =============================================================================
+// Description: Gravity compensation feed forward for lever arm
+// Author: FRC Team 3512, Spartatroniks
+// =============================================================================
+
+#include "LeverPIDController.hpp"
+
+LeverPIDController::LeverPIDController(float p,
+                                       float i,
+                                       float d,
+                                       float v,
+                                       float a,
+                                       float f,
+                                       PIDSource* source,
+                                       PIDOutput* output,
+                                       float period) :
+    PIDController(0.f, 0.f, 0.f, 0.f, 0.f,
+                  source,
+                  output,
+                  period) {
+    m_F = f;
+}
+
+double LeverPIDController::CalculateFeedForward() {
+    return GetV() * GetSetpoint().velocity + GetA() *
+           GetSetpoint().acceleration;
+}
+
+void LeverPIDController::SetF(double f) {
+    m_F = f;
+}
