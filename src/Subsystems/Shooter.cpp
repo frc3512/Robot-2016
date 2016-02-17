c08e5fad92e13ebc42f33c1f1822ae21d061a819
KeepEnemiesCloser <codelyokoliker@gmail.com>
2016-02-16 20:35:36 -0800

    Merge shooter state machines
    Change-Id: Ice404eea19c581049a923332b44f87a376335b38

--- src/Subsystems/Shooter.cpp
+++ src/Subsystems/Shooter.cpp
@@ -24,9 +24,11 @@ Shooter::Shooter() {
     m_rightShootGrbx.SetPIDSourceType(PIDSourceType::kRate);
 
     m_joystickEvent.RegisterButtonEvent("PressedIntakeButton",
-                                        k_shootStickPort, 1, true);
-    m_joystickEvent.RegisterButtonEvent("PressedShooterButton",
                                         k_shootStickPort, 2, true);
+    m_joystickEvent.RegisterButtonEvent("ReleasedIntakeButton",
+                                        k_shootStickPort, 2, false);
+    m_joystickEvent.RegisterButtonEvent("PressedShooterButton",
+                                        k_shootStickPort, 1, true);
     m_dioEvent.RegisterInputEvent("BallLoaded", k_intakeLimitPin, true, false,
                                   m_shootSM);
     m_dioEvent.RegisterInputEvent("ShooterZeroed",
@@ -42,36 +44,12 @@ Shooter::Shooter() {
         m_rightShootGrbx.Set(0);
         m_rollBallGrbx.Set(0);
     };
-    state->CheckTransition = [] (const std::string& event) {
-                                 if (event == "PressedShooterButton") {
-                                     return "StartIntake";
-                                 }
-                                 else {
-                                     return "";
-                                 }
-                             };
-    m_intakeSM.AddState(std::move(state));
-
-    // Start intake
-    state = std::make_unique<State>("StartIntake");
-    state->Run = [this] {
-        m_rollBallGrbx.Set(-.5);
-    };
-    state->CheckTransition = [] (const std::string& event) {
-                                 if (event == "BallLoaded") {
-                                     return "Idle";
-                                 }
-                                 else {
-                                     return "";
-                                 }
-                             };
-    m_intakeSM.AddState(std::move(state));
-
-    // Idle
-    state = std::make_unique<State>("Idle");
     state->CheckTransition = [this] (const std::string& event) {
                                  if (event == "PressedIntakeButton") {
                                      return "StartIntake";
+                                 }
+                                 else if (event == "PressedShooterButton") {
+                                     return "SpinningUpMotors";
                                  }
                                  else if (event == "ShooterZeroed") {
                                      m_shooterHeightGrbx.ResetEncoder();
@@ -83,34 +61,65 @@ Shooter::Shooter() {
                              };
     m_shootSM.AddState(std::move(state));
 
-    // Start intake and shooter
-    state = std::make_unique<State>("StartIntakeAndShooter");
+    // Start intake
+    state = std::make_unique<State>("StartIntake");
+    state->Run = [this] {
+        m_rollBallGrbx.Set(-0.5);
+        m_leftShootGrbx.Set(-0.5);
+        m_rightShootGrbx.Set(-0.5);
+    };
+    state->CheckTransition = [] (const std::string& event) {
+                                 if (event == "ReleasedIntakeButton") {
+                                     return "Idle";
+                                 }
+                                 else if (event == "BallLoaded") {
+                                     return "Idle";
+                                 }
+
+                                 else {
+                                     return "";
+                                 }
+                             };
+    m_shootSM.AddState(std::move(state));
+
+    state = std::make_unique<State>("SpinningUpMotors");
+    state->Entry = [this] {
+    	m_timerEvent.Reset();
+    };
+    state->Run = [this] {
+    	m_leftShootGrbx.Set(m_manualShooterSpeed);
+    	m_rightShootGrbx.Set(m_manualShooterSpeed);
+    };
+    state->CheckTransition = [this] (const std::string& event) {
+    	if (event == "ShootTimer") {
+    		return "StartShooter";
+    	}
+    	else {
+    		return "";
+    	}
+    };
+
+    // Start shooter
+    state = std::make_unique<State>("StartShooter");
     state->Entry = [this] {
         m_timerEvent.Reset();
     };
     state->Run = [this] {
-        m_leftShootGrbx.Set(-.5);
-        m_rightShootGrbx.Set(-.5);
-        m_rollBallGrbx.Set(.75);
+        m_rollBallGrbx.Set(1);
+        m_leftShootGrbx.Set(m_manualShooterSpeed);
+        m_rightShootGrbx.Set(m_manualShooterSpeed);
     };
     state->CheckTransition = [this] (const std::string& event) {
                                  if (event == "ShootTimer") {
                                      return "Idle";
                                  }
-                                 else if (event == "ShooterZeroed") {
-                                     m_shooterHeightGrbx.ResetEncoder();
-                                     return "";
-                                 }
                                  else {
                                      return "";
                                  }
                              };
-    state->Exit = [this] {
-        // Stop intake motor
-        m_rollBallGrbx.Set(0);
-    };
     m_shootSM.AddState(std::move(state));
 }
+
 
 int32_t Shooter::GetShootHeightValue() const {
     return m_shooterHeightGrbx.Get();
@@ -121,13 +130,11 @@ void Shooter::ToggleManualOverride() {
 }
 
 void Shooter::UpdateState() {
-    m_intakeSM.Run();
-    m_joystickEvent.Poll(m_intakeSM);
-    m_timerEvent.Poll(m_intakeSM);
-
     m_shootSM.Run();
     m_joystickEvent.Poll(m_shootSM);
-    m_timerEvent.Poll(m_intakeSM);
+    m_timerEvent.Poll(m_shootSM);
+    std::cout<< "STATE: "<< m_shootSM.StackTrace() << std::endl;
+
 }
 
 bool Shooter::GetManualOverride() const {
@@ -153,8 +160,7 @@ void Shooter::SetManualShooterSpeed(double speed) {
     m_leftShootPID->Disable();
     m_rightShootPID->Disable();
 
-    m_leftShootGrbx.Set(speed);
-    m_rightShootGrbx.Set(speed);
+    m_manualShooterSpeed = speed;
 }
 
 /* Conversion table for calculating RPM
@@ -168,16 +174,16 @@ float Shooter::GetLeftRPM() const {
     // TODO: document magic number math
     // std::cout << "Left Motor Raw Output: " << m_leftShootGrbx.GetSpeed() <<
     //    std::endl;
-    return m_leftShootGrbx.GetSpeed() * 5.0f * 1000.0f * 60.0f * 2.0f /
-           (100.0f * 360.0f);
+    return m_leftShootGrbx.GetSpeed(); // * 5.0f * 1000.0f * 60.0f * 2.0f /
+           //(100.0f * 360.0f);
 }
 
 float Shooter::GetRightRPM() const {
     // TODO: document magic number math
     // std::cout << "Right Motor Raw Output: " << m_rightShootGrbx.GetSpeed() <<
     //    std::endl;
-    return m_rightShootGrbx.GetSpeed() * 5.0f * 1000.0f * 60.0f * 2.0f /
-           (100.0f * 360.0f);
+    return m_rightShootGrbx.GetSpeed(); //* 5.0f * 1000.0f * 60.0f * 2.0f /
+           //(100.0f * 360.0f);
 }
 
 void Shooter::ReloadPID() {
