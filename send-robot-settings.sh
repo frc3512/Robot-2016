042dcf13499c4f94d122456b1e9038c3cc9e312f
Tyler Veness <calcmogul@gmail.com>
2016-02-20 11:04:59 -0800

    Removed RobotSettings.txt
    Change-Id: I6b1e166a5c292d912ab76506240a34d6057fcd49

--- send-robot-settings.sh
+++ send-robot-settings.sh
@@ -3,9 +3,9 @@ HOST='10.35.12.2'
 USER='lvuser'
 
 # Add file names here to send them
-FILESRC=('RobotSettings.txt' 'GUISettings.txt')
-PATHDEST=('/home/lvuser' '/home/lvuser')
-FILEDEST=('RobotSettings.txt' 'GUISettings.txt')
+FILESRC=('GUISettings.txt')
+PATHDEST=('/home/lvuser')
+FILEDEST=('GUISettings.txt')
 
 # script: send files in list
 for (( i=0 ; i < ${#FILESRC[@]} ; i++ ));
