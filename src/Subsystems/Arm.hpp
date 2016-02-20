52c9c61318ddbc7acebafaaf52fadab752d4fb4c
Nicholas DeBruno <nadebruno@yahoo.com>
2016-02-20 13:33:00 -0800

    Added arm state machine
    Change-Id: I8a0d219ca64e8201e60858a95867e66deeb2edc1

--- src/Subsystems/Arm.hpp
+++ src/Subsystems/Arm.hpp
@@ -10,9 +10,13 @@
 #include <Timer.h>
 
 #include "../Constants.hpp"
+#include "../Events/JoystickEventGenerator.hpp"
+#include "../Events/DigitalInputEventGenerator.hpp"
+#include "../Events/TimerEventGenerator.hpp"
 #include "../MotionProfile/TrapezoidProfile.hpp"
 #include "GearBox.hpp"
 #include "SubsystemBase.hpp"
+
 
 class PIDController;
 class TrapezoidProfile;
@@ -24,6 +28,10 @@ public:
     void ReloadPID();
     void ResetEncoders();
 
+    void SetCarryingHeight(double speed);
+    void SetClimbHeight();
+    bool GetManualOverride();
+
     void SetManualArmHeight(double height);
     void SetPIDArmHeight(double height);
     void SetManualCarriagePosition(double position);
@@ -31,6 +39,13 @@ public:
     void UpdateState();
 
 private:
+    bool m_manual = true;
+    double m_manualArmSpeed = 0.0;
+
+    JoystickEventGenerator m_joystickEvent;
+    DigitalInputEventGenerator m_dioEvent;
+    TimerEventGenerator m_timerEvent{"LiftTime", 3.0}; // TODO: change LiftTime value
+
     GearBox m_leftArmActuator{-1, k_leftArmLiftID};
     std::shared_ptr<PIDController> m_leftArmPID;
     std::shared_ptr<TrapezoidProfile> m_leftArmProfile;
@@ -48,6 +63,8 @@ private:
 
     std::shared_ptr<TrapezoidProfile> m_rightArmHeightProfile;
     std::shared_ptr<TrapezoidProfile> m_leftArmHeightProfile;
+
+    StateMachine m_armSM{"ArmSM"};
 };
 
 #endif // ARM_HPP
