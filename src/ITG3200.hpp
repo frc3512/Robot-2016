43cdf885cd64b8fa99907e2e24d6c87a4efb84ba
Tyler Veness <calcmogul@gmail.com>
2016-03-07 22:57:54 -0800

    Wrote class for interfacing with an ITG3200 gyroscope
    Change-Id: Icab6e6935d087acb8bb3a466aac590cc9f5815f2

---
+++ src/ITG3200.hpp
@@ -0,0 +1,80 @@
+// =============================================================================
+// Description: Provides access to an ITG3200 3-axis digital gyroscope via the
+//              I2C bus
+// Author: FRC Team 3512, Spartatroniks
+// =============================================================================
+
+#ifndef ITG3200_HPP
+#define ITG3200_HPP
+
+#include <cstdint>
+#include <atomic>
+
+#include <GyroBase.h>
+#include <I2C.h>
+#include <Notifier.h>
+
+class ITG3200 : public I2C, public GyroBase {
+public:
+    ITG3200(Port port, uint32_t deviceAddress = k_address);
+
+    double GetAngleX() const;
+    double GetAngleY() const;
+    double GetAngleZ() const;
+
+    double GetRateX();
+    double GetRateY();
+    double GetRateZ();
+
+    void Reset();
+    void Calibrate();
+
+private:
+    static constexpr uint8_t k_address = 0x69;
+
+    static constexpr uint8_t k_samplePeriodDividerRegister = 0x15;
+    static constexpr uint8_t k_digitalLowPassFilterRegister = 0x16;
+    static constexpr uint8_t k_gyroXOutRegister = 0x1D;
+    static constexpr uint8_t k_gyroYOutRegister = 0x1F;
+    static constexpr uint8_t k_gyroZOutRegister = 0x21;
+
+    // Digital Low Pass Filter (DLPF), Full Scale Register Bits
+    // FS_SEL must be set to 3 for proper operation
+    static constexpr uint8_t DLPF_CFG_0 = 1 << 0;
+    static constexpr uint8_t DLPF_CFG_1 = 1 << 1;
+    static constexpr uint8_t DLPF_CFG_2 = 1 << 2;
+    static constexpr uint8_t DLPF_FS_SEL_0 = 1 << 3;
+    static constexpr uint8_t DLPF_FS_SEL_1 = 1 << 4;
+
+    static constexpr double k_calibrationSampleTime = 2.0;
+    static constexpr double k_samplePeriod = 0.01;
+    static constexpr int16_t k_numCalibrationSamples = k_calibrationSampleTime /
+                                                       k_samplePeriod;
+
+    // Sensitivity of gyro in least significant bits per degree/second
+    static constexpr double k_degreesPerSecondPerLSB = 1.0 / 14.375;
+
+    std::atomic<double> m_xAngle{0.0};
+    std::atomic<double> m_yAngle{0.0};
+    std::atomic<double> m_zAngle{0.0};
+
+    double m_xZero = 0.0;
+    double m_yZero = 0.0;
+    double m_zZero = 0.0;
+
+    Notifier m_loop{&ITG3200::Calculate, this};
+
+    // Used by notifier to update angle estimate
+    void Calculate();
+
+    // Read raw X axis from gyro
+    int16_t GetRawX();
+
+    // Read raw Y axis from gyro
+    int16_t GetRawY();
+
+    // Read raw Z axis from gyro
+    int16_t GetRawZ();
+};
+
+#endif // ITG3200_HPP
